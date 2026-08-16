3b4f9569fb001fb9bff3afeb300dae153dd49f65
makombe <kennedymakombe@gmail.com>
2026-08-16 20:21:57 +0300

    More fixes

--- api/src/main/java/org/openmrs/module/stockmanagement/api/impl/StockManagementServiceImpl.java
+++ api/src/main/java/org/openmrs/module/stockmanagement/api/impl/StockManagementServiceImpl.java
@@ -44,8 +44,6 @@ import org.openmrs.util.OpenmrsConstants;
 import org.openmrs.util.PrivilegeConstants;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
